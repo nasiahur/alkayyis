8582c5c70646eb10d2a37b3dc95b3e0f946687f9
Nikmatun Aliyah Salsabila <nikmatunaliyahsalsabila@gmail.com>
2017-07-23 00:36:14 -0700

    CONFIG DATABASE

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+	//defined('ROOT') or die('');
+	
+	define('HOST','localhost');
+	define('USERNAME','root');
+	define('PASS','');
+	define('DBNAME','alkayyis');
+	
+	$conn = mysqli_connect(HOST,USERNAME,PASS,DBNAME);	
+?>
