294f8adde88a5fc161e9b746744595cdc11d8125
Nikmatun Aliyah Salsabila <nikmatunaliyahsalsabila@gmail.com>
2017-07-23 00:52:14 -0700

    level kuis

---
+++ quiz.php
@@ -0,0 +1,131 @@
+<?php include "base.html";?>
+
+<div class="sc_parallax_content" style="background-image:url(images/kuisbackground.png); background-position:50% 50%;height:180px;margin-top:-100px;">
+	<div style="margin-left:90px">
+		<div class="sc_content main animated" style="margin-top:129px !important;margin-bottom:172px !important;">
+			<div class="columnsWrap sc_columns sc_columns_count_2">
+				<div class="columns1_2 sc_column_item sc_column_item_1 odd first"></div><div class="columns1_2 sc_column_item sc_column_item_2 even"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="mainWrap without_sidebar">
+<div class="main" role="main">
+<div class="content">
+<div class="vc_row wpb_row vc_row-fluid">
+	<div class="wpb_column vc_column_container vc_col-sm-12">
+		<div class="vc_column-inner " >
+			<div class="wpb_wrapper" >
+				<div class="isotopeFiltr"></div>
+				<div class="sc_content main animated" style="margin-top:74px !important;margin-bottom:66px !important;">
+					<div class="columnsWrap sc_columns sc_columns_count_5">
+						<div class="columns1_5 sc_column_item sc_column_item_1 odd first" style="text-align:none;">
+						<a href="kuis_3.php?id=1">
+							<div class="sc_title_icon sc_title_top sc_size_huge">
+								<img src="images/openkuis.png" alt="" />
+							</div>
+							<center><h6 class="sc_title sc_title_iconed" style="color:salmon">1</h6></center></a>
+						</div>
+						<div class="columns1_5 sc_column_item sc_column_item_2 even">
+						<a href="kuis_3.php?id=2">
+							<div class="sc_title_icon sc_title_top sc_size_huge">
+								<img src="images/openkuis.png" alt="" />
+							</div>
+							<center><h6 class="sc_title sc_title_iconed" style="color:salmon">2</h6></center></a>
+						</div>
+						<div class="columns1_5 sc_column_item sc_column_item_3 odd first" style="text-align:none;">
+						<a href="kuis_3.php?id=3">
+							<div class="sc_title_icon sc_title_top sc_size_huge" style="border:2px,solid, black">
+								<img src="images/padlock_.png" alt="" />
+							</div>
+							<center><h6 class="sc_title sc_title_iconed" style="color:red">3</h6></center></a>
+						</div>
+						<div class="columns1_5 sc_column_item sc_column_item_4 even">
+						<a href="kuis_3.php?id=4">
+							<div class="sc_title_icon sc_title_top sc_size_huge">
+								<img src="images/padlock1.png" alt="" />
+							</div>
+							<center><h6 class="sc_title sc_title_iconed" style="color:red">4</h6></center></a>
+						</div>
+						<div class="columns1_5 sc_column_item sc_column_item_5 odd">
+						<a href="kuis_3.php?id=5">
+							<div class="sc_title_icon sc_title_top sc_size_huge">
+								<img src="images/padlock2.png" alt="" />
+							</div>
+							<center><h6 class="sc_title sc_title_iconed" style="color:red">5</h6></center></a>
+						</div>
+						<div class="columns1_5 sc_column_item sc_column_item_1 odd first" style="text-align:none;">
+						<a href="kuis_3.php?id=6">
+							<div class="sc_title_icon sc_title_top sc_size_huge">
+								<img src="images/padlock3.png" alt="" />
+							</div>
+							<center><h6 class="sc_title sc_title_iconed" style="color:red">6</h6></center></a>
+						</div>
+						<div class="columns1_5 sc_column_item sc_column_item_2 even">
+						<a href="kuis_3.php?id=7">
+							<div class="sc_title_icon sc_title_top sc_size_huge">
+								<img src="images/padlock4.png" alt="" />
+							</div>
+							<center><h6 class="sc_title sc_title_iconed" style="color:red">7</h6></center></a>
+						</div>
+						<div class="columns1_5 sc_column_item sc_column_item_3 odd first" style="text-align:none;">
+						<a href="kuis_3.php?id=8">
+							<div class="sc_title_icon sc_title_top sc_size_huge">
+								<img src="images/padlock1.png" alt="" />
+							</div>
+							<center><h6 class="sc_title sc_title_iconed" style="color:red">8</h6></center></a>
+						</div>
+						<div class="columns1_5 sc_column_item sc_column_item_4 even">
+						<a href="kuis_3.php?id=9">
+							<div class="sc_title_icon sc_title_top sc_size_huge">
+								<img src="images/padlock2.png" alt="" />
+							</div>
+							<center><h6 class="sc_title sc_title_iconed" style="color:red">9</h6></center></a>
+						</div>
+						<div class="columns1_5 sc_column_item sc_column_item_5 odd">
+						<a href="kuis_3.php?id=10">
+							<div class="sc_title_icon sc_title_top sc_size_huge">
+								<img src="images/padlock3.png" alt="" />
+							</div>
+							<center><h6 class="sc_title sc_title_iconed" style="color:red">10</h6></center></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<script type="text/javascript">
+				var ppp = 12;
+				jQuery(document).ready(function () {
+					jQuery(".isotopeFiltr").append('<ul><li class="squareButton active"><a href="#" data-filter="*">Campur</a></li><li class="squareButton"><a href="#" data-filter=".flt_71">Akhlak di Rumah</a></li><li class="squareButton"><a href="#" data-filter=".flt_66">Akhlak di Sekolah</a></li><li class="squareButton"><a href="#" data-filter=".flt_61">Berteman</a></li><li class="squareButton"><a href="#" data-filter=".flt_64">Berpetualang</a></li></ul>');
+				});
+			</script>
+		</div>
+	</div>
+</div>
+	<div id="pagination" class="pagination">
+				<ul class="pageLibrary">
+					<li class="pager_last ico right squareButton light"><a href="page/2/index.html">Previous </a></li>
+					<li class="pager_pages libPage">Page 
+						<input class="navInput" readonly="readonly" type="text" size="1" value="1"> of 2
+						<div id="pageNavSlider" class="boxShadow pageFocusBlock navPadding">
+							<div class="sc_slider sc_slider_swiper sc_slider_controls sc_slider_controls_top sc_slider_nopagination sc_slider_noautoplay swiper-slider-container">
+								<ul class="slides swiper-wrapper" data-current-slide="1">
+									<li class="swiper-slide">
+										<div class="pageNumber"><table><tr><td><a href="#" class="active">1</a></li><td><a href="page/2/index.html">2</a></td></tr></table>
+										</div>
+									</li>
+								</ul>
+							</div>
+						<ul class="flex-direction-nav"><li><a class="flex-prev" href="#"></a></li><li><a class="flex-next" href="#"></a></li></ul>
+						</div>
+					</li>
+					<li class="pager_next ico right squareButton light"><a href="page/2/index.html">Next </a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+</div> <!-- </div> class="main" role="main"> -->				
+</div> <!-- /.mainWrap -->
